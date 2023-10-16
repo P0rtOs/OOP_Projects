7c00c3c1003fa1a2509334177c597f95510d24f1
P0rtOs <nsimkov@gmail.com>
2023-10-16 11:49:29 +0300

    start of project

---
+++ LAB1.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <vector>
+
+class Manager 
+{
+public:
+    Manager() {}
+
+    // Метод для планування маршруту для транспортного засобу
+    void planRoute(Vehicle& vehicle, const Point& destination, const std::vector<Point>& stops) 
+    {
+        // Логіка для планування маршруту
+    }
+
+    // Метод для запуску симуляції руху
+    void startSimulation() 
+    {
+        // Логіка для запуску симуляції
+    }
+
+    // Метод для відстеження руху транспортних засобів
+    void trackVehicleMovement() 
+    {
+        // Логіка для відстеження руху
+    }
+
+    // Метод для завершення симуляції
+    void endSimulation() 
+    {
+        // Логіка для завершення симуляції
+    }
+};
+
+int main()
+{
+    
+}
