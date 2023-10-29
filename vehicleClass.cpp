63bc7862364c3dc256493003525850bb736b3317
P0rtOs <nsimkov@gmail.com>
2023-10-29 15:09:54 +0200

    Додав декілька видів машин
    Реалізував наслідування для різних видів вантажівок

--- vehicleClass.cpp
+++ vehicleClass.cpp
@@ -41,3 +41,52 @@ void Vehicle::setVehicleType(std::string thisType) {
 std::string Vehicle::getVehicleType() {
 	return this->vehicleType;
 }
+
+
+Car::Car() : numberOfDoors(4) {}
+Car::Car(int vehicleId, Road& vehicleRoad, std::string vehicleType, int doors)
+	: Vehicle(vehicleId, vehicleRoad, vehicleType), numberOfDoors(doors) {}
+
+int Car::getNumberOfDoors() {
+    return numberOfDoors;
+}
+
+void Car::setNumberOfDoors(int doors) {
+    numberOfDoors = doors;
+}
+
+
+Truck::Truck() : aim(default_point) {} //конструткор по стандарту для цілі вантажівки, можливо якийсь центр логістики
+Truck::Truck(int vehicleId, Road& vehicleRoad, std::string vehicleType, Point point)
+	: Vehicle(vehicleId, vehicleRoad, vehicleType), aim(point) {}
+
+Point Truck::getAim() {
+    return aim;
+}
+
+void Truck::setAim(Point point) {
+    aim = point;
+}
+
+
+
+School_bus::School_bus() : bus_stations_to_serve(default_vector), school_to_serve(default_point) {}
+School_bus::School_bus(int vehicleId, Road& vehicleRoad, std::string vehicleType, Point school_to_serve1, std::vector<Point> bus_stations_to_serve1)
+    : Vehicle(vehicleId, vehicleRoad, vehicleType), school_to_serve(school_to_serve1), bus_stations_to_serve(bus_stations_to_serve1) {}
+
+Point School_bus::getSchool() {
+    return school_to_serve;
+}
+
+void School_bus::setSchool(Point school_to_serve1) {
+    school_to_serve = school_to_serve1;
+}
+
+
+std::vector<Point> School_bus::getbus_stations_to_serve() {
+    return bus_stations_to_serve;
+}
+
+void School_bus::setbus_stations_to_serve(std::vector<Point> bus_stations_to_serve1) {
+    bus_stations_to_serve = bus_stations_to_serve1;
+}
--- vehicleClass.h
+++ vehicleClass.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <iostream>
 #include "roadClass.h"
+#include <vector>
 
 class Vehicle {
 	private:
@@ -21,4 +22,46 @@ class Vehicle {
 		std::string getVehicleType();
 
 };
+
+class Car : public Vehicle {  //плануЇтьс€ €к звичайна базова одиниц€ без приоритет≥в чи особливостей.
+private:
+    int numberOfDoors;
+
+public:
+    Car();
+    Car(int vehicleId, Road& vehicleRoad, std::string vehicleType, int doors);
+    int getNumberOfDoors();
+    void setNumberOfDoors(int doors);
+};
+
+class Truck : public Vehicle {  //плануЇтьс€ €к звичайна базова одиниц€ без приоритет≥в чи особливостей.
+private:
+    Point aim;
+
+    Point default_point;
+
+public:
+    Truck();    //конструткор по стандарту дл€ ц≥л≥ вантаж≥вки, можливо €кийсь центр лог≥стики
+    Truck(int vehicleId, Road& vehicleRoad, std::string vehicleType, Point point);
+    Point getAim();
+    void setAim(Point point);
+};
+
+class School_bus : public Vehicle {  
+private:
+    std::vector<Point> bus_stations_to_serve;
+    Point school_to_serve;
+
+    Point default_point;
+    std::vector<Point> default_vector;
+
+public:
+    School_bus();
+    School_bus(int vehicleId, Road& vehicleRoad, std::string vehicleType, Point school_to_serve1, std::vector<Point> bus_stations_to_serve1);
+    Point getSchool();
+    void setSchool(Point school_to_serve1);
+    std::vector<Point> getbus_stations_to_serve();
+    void setbus_stations_to_serve(std::vector<Point> bus_stations_to_serve1);
+};
+
 
