afea4b304167131a939b6fdab1eb767ab4f23add
P0rtOs <nsimkov@gmail.com>
2023-12-26 01:28:24 +0200

    Додано початкову версію зчитування файлів

--- fileManager.h
+++ fileManager.h
@@ -1,26 +1,57 @@
-//#pragma once
-//#include "vehicleManager.h"
-//#include <fstream>
-//#include <sstream>
-//
-//std::vector<Point> readPointsFromFile(const std::string& filename) {
-//    std::vector<Point> points;
-//    std::ifstream file(filename);
-//
-//    if (file.is_open()) {
-//        int pointId;
-//        double pointX, pointY;
-//
-//       
-//        file.close();
-//    }
-//
-//    return points;
-//}
-//class fileManager {
-//    private:
-//        int chosenFilesNumber;
-//        bool isReadAlready;
-//
-//};
+#pragma once
+
+#include "vehicleManager.h"
+#include "connectionClass.h"
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+//TODO Дописати конструктор для поінта з ініціалізацією всіх полів. Перевірити правильність зчитування 
+// Додати створення точки менеджером після зчитування даних.
+class readingFiles
+{
+    void readDataFromFile(const std::string& filename)
+    {
+        const std::string a = "TEST1.txt";
+        std::ifstream file(a);
+        int id, x, y, readed_int, ticks, mas_limit;
+        std::vector<Connection> connections;
+        //std::ifstream file(filename);
+
+        if (!file.is_open())
+        {
+            std::cerr << "Unable to open the file: " << filename << std::endl;
+            exit(0);
+        }
+
+        std::string line;
+        while (std::getline(file, line))
+        {
+            std::istringstream iss(line);
+            char symbol;
+            iss >> symbol;
+
+            if (symbol == '#')
+            {
+                iss >> id >> x >> y;
+
+                int i = 0;
+                while (iss >> symbol && symbol != '?')
+                {
+                    iss >> readed_int;
+                    connections[i].setNeighborId(readed_int);
+                    i++;
+                }
+                
+                iss >> ticks >> mas_limit;
+            }
+
+            Point point(id, x, y, connections);
+        }
+
+        file.close();
+    }
+};
+
 
