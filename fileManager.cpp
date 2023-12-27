6b203de8e4e7d2d88fc3f3aa2fe06fde78ed3434
P0rtOs <nsimkov@gmail.com>
2023-12-27 16:01:25 +0200

    Додано симуляцію та доповнено файл

--- fileManager.cpp
+++ fileManager.cpp
@@ -1,6 +1,6 @@
 #include "fileManager.h"
 
-void readingFiles::readPointsFromFile(const std::string& filename)
+/*void readingFiles::readPointsFromFile(const std::string& filename)
 {
     char ch;
     int id, x, y, neightbourAmount, readed_int, ticks, mas_limit;
@@ -45,21 +45,78 @@ void readingFiles::readPointsFromFile(const std::string& filename)
                 file >> readed_int;
                 connections[i]->setNeighborId(readed_int);
             }
-            for (int i = 0; i < neightbourAmount; i++)
+            for (int s = 0; s < neightbourAmount; s++)
             {
                 text >> ticks;
-                connections[i]->setTicksToTraverse(ticks);
+                connections[s]->setTicksToTraverse(ticks);
             }
-            for (int i = 0; i < neightbourAmount; i++)
+            for (int j = 0; j < neightbourAmount; j++)
             {
                 text >> mas_limit;
-                connections[i]->setWeightLimit(mas_limit);
+                connections[j]->setWeightLimit(mas_limit);
             }
-            globalPointFactory.createPoint(id, x, y, connections);
+            globalPointFactory.createPoint(id, x, y, connections, neightbourAmount);
         }
         
     }
+}*/
+
+void handlePointCreation(int id, int x, int y, const std::vector<int>& neighborIds, const std::vector<int>& ticks, const std::vector<int>& weights) {
+    // Create the Point object (modify according to your Point class constructor)
+    Point* point = new Point(id, x, y);
+
+    // Create and add connections
+    for (size_t i = 0; i < neighborIds.size(); ++i) {
+        point->addNeighbor(neighborIds[i], ticks[i], weights[i]);
+    }
+
+    std::cout << "id:" << id << " " << x << " " << y;
+    for (int i = 0; i < neighborIds.size(); i++)
+    {
+        std::cout << " N id " << point->getNeighbor()[i]->getNeighborId() << " ticks: " << point->getNeighbor()[i]->getTicksToTraverse() << " weight: " << point->getNeighbor()[i]->getWeightLimit() << std::endl;
+    }
+    // Add point to the globalPointManager or other relevant data structures
+    globalPointManager.addPoint(point);
 }
+void readingFiles::readPointsFromFile(const std::string& filename) {
+    std::ifstream file("points_pack_1.txt");
+    if (!file.is_open()) {
+        std::cerr << "Unable to open the file: " << filename << std::endl;
+        return;
+    }
+
+    std::string line;
+    while (std::getline(file, line)) {
+        if (line.empty() || line[0] != '#') continue; // Skip non-data lines
+
+        std::istringstream iss(line.substr(1)); // Skip '#'
+
+        int id, x, y, numberOfConnections;
+        iss >> id >> x >> y >> numberOfConnections;
+
+        std::vector<int> neighborIds(numberOfConnections);
+        std::vector<int> ticks(numberOfConnections);
+        std::vector<int> weights(numberOfConnections);
+
+        for (int& neighborId : neighborIds) {
+            iss >> neighborId;
+        }
+
+        for (int& tick : ticks) {
+            iss >> tick;
+        }
+
+        for (int& weight : weights) {
+            iss >> weight;
+        }
+
+        // Assuming a function to handle the creation of Points and their connections
+        // This function should take the ID, coordinates, and a list of connections
+        handlePointCreation(id, x, y, neighborIds, ticks, weights);
+    }
+}
+
+
 
 
 
--- pointFactory.h
+++ pointFactory.h
@@ -5,7 +5,7 @@ class PointFactory {
 public:
     // Метод для створення стандартного Point
     Point* createPoint(int pointId, double x, double y);
-    Point* createPoint(int pointId, double x, double y, std::vector<Connection*> connection);
+    Point* createPoint(int pointId, double x, double y, std::vector<Connection*>& connection, int neightbourAmount);
 
     // Тут можна додати додаткові методи для створення різних типів Point
 };
