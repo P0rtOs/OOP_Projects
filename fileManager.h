d0c2a06db11b958dbe803beffeee273f1b7931b9
P0rtOs <nsimkov@gmail.com>
2023-12-27 10:29:33 +0200

    Зробив реалізацію файл менеджеру коректною

--- fileManager.h
+++ fileManager.h
@@ -13,65 +13,9 @@
 // Додати створення точки після зчитування даних.
 class readingFiles
 {
-    void readPointsFromFile(const std::string& filename)
-    {
-        const std::string a = "points_pack_1.txt";
-        std::ifstream file(a);
-        int id, x, y, readed_int, ticks, mas_limit;
-        std::vector<Connection*> connections(5);
-        //std::ifstream file(filename);
+    void readPointsFromFile(const std::string& filename);
 
-        if (!file.is_open())
-        {
-            std::cerr << "Unable to open the file: " << filename << std::endl;
-            exit(0);
-        }
-
-        std::string line;
-        while (std::getline(file, line))
-        {
-            std::istringstream iss(line);
-            char symbol;
-            iss >> symbol;
-
-            if (symbol == '#')
-            {
-                iss >> id >> x >> y;
-
-                int i = 0;
-                while (iss >> symbol && symbol != '?')
-                {
-                    iss >> readed_int;
-                    if (connections.size() == i)
-                    {
-                        connections.resize(i + 5);
-                    }
-                    connections[i]->setNeighborId(readed_int);
-                    i++;
-                }
-                
-                iss >> ticks;
-                iss >> mas_limit;
-                for (int s = 0; s < connections.size(); s++)
-                {
-                    connections[s]->setTicksToTraverse(ticks);
-                    connections[s]->setWeightLimit(mas_limit);
-                }
-            }
-
-            globalPointFactory.createPoint(id, x, y, connections);
-        }
-
-
-
-        file.close();
-    }
-
-    void readVehiclesFromFile(const std::string& filename)
-    {
-
-
-    }
+    void readVehiclesFromFile(const std::string& filename);
 };
 
 
